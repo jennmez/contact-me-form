5b9145c384e534b4738bb7f6eaca95f12c384a11
jennmez <jennmez@gmail.com>
2020-10-04 16:05:38 -0500

    updated with new skeleton, still can't get name label to move

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-import Contact from './Contact.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Contact />
-      </header>
+      <form action="">
+        <fieldset>
+          <legend>Write in your name</legend>
+          <div className="field-outlined">
+            <input id="name" type="text" className="input" required />
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+            {/* <span className="line"></span> */}
+          </div>
+        </fieldset>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
--- src/Contact.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Contact from './Contact';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Contact />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
